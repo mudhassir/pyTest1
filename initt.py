73aff79f6291c77bc4f7a20a446ae47715109577
mudhassir <mudhassir@gmail.com>
2025-11-24 18:46:08 -0500

    test

--- initt.py
+++ initt.py
@@ -1,2 +1,8 @@
+# ...existing code...
 print("hello world!")
 print("hello Aariz!")
+# added code: print table of 5
+print("\nTable of 5:")
+for i in range(1, 11):
+    print(f"5 x {i} = {5 * i}")
+# ...existing code...
